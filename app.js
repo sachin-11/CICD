10c6a755b398b57511a214e03cfee11ff4495934
sachin <sachin.singh@manektech.com>
2023-02-03 13:20:12 +0530

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('this is home page')
+})
+
+const PORT = process.env.PORT || 8080
+
+
+app.listen(PORT, () => {
+    console.log(`Server is started on port ${PORT}`)
+})
